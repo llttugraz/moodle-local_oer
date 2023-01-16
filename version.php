8f108bf4009fac82544fa1612558d8925cdc8cbc
Christian Ortner <christian.ortner@tugraz.at>
2023-01-16 16:56:34 +0100

    add RC7 to version

--- version.php
+++ version.php
@@ -28,6 +28,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version      = 2022111700;
 $plugin->requires     = 2020061500;
 $plugin->component    = 'local_oer';
-$plugin->release      = 'v2.1.14-RC6';
+$plugin->release      = 'v2.1.14-RC7';
 $plugin->maturity     = MATURITY_RC;
 $plugin->dependencies = [];
